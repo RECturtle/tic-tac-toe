00dd682d58451c2d36ab82074797a4fa467f3bf2
RECturtle <spencer.mertes@gmail.com>
2022-02-15 20:14:39 -0500

    add code

---
+++ ticktactoe.rb
@@ -0,0 +1,103 @@
+class TickTacToe
+    LINES = [0,1,2,3,4,5, 6,7,8, 0,3,6, 1,4,7, 2,5,8, 0,4,8, 2,4,6]
+
+    def initialize(player1, player2)
+        @board = [1,2,3,4,5,6,7,8,9]
+        @player1 = player1
+        @player2 = player2
+        gameplay
+    end
+
+    def gameplay
+        player = @player1
+        spaces = [1,2,3,4,5,6,7,8,9]
+        game_over = false
+        round = 1
+
+        while !game_over
+            puts ""
+            puts "Round Number: #{round}"
+            print_board
+            puts "#{player.name}: Your turn!"
+            position = gets.chomp.to_i
+
+            if spaces.include?(position)
+                spaces.delete(position)
+                @board[position-1] = player.symbol
+
+                if win_check(player)
+                    puts ""
+                    puts "======================"
+                    puts ""
+                    puts "#{player.name} wins!"
+                    print_board
+                    game_over = true
+                end
+                round += 1
+                player == @player1 ? player = @player2 : player = @player1
+            end
+        end
+    end
+
+    def win_check(player)
+        arr = []
+        LINES.each do |position|
+            arr << @board[position]
+        end
+        if arr.join =~ /#{player.symbol}{3}/
+            return true
+        end
+        return false
+    end
+
+    def print_board
+        puts ""
+        row1 = " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        row2 = " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        row3 = " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+        divider ="---+---+---"
+        puts row1
+        puts divider
+        puts row2
+        puts divider
+        puts row3
+        puts ""  
+    end
+end
+
+class Player
+    attr_accessor :name, :symbol
+    @@picked_symbol = ""
+    @@one_player = false
+
+    def initialize
+        @name = get_name()
+        @symbol = get_symbol()
+    end
+
+    def get_name
+        puts @@one_player == false ? "Player 1: What's your name?" : "Player 2: What's your name?"
+        @@one_player = true
+        return gets.chomp
+    end
+
+    def get_symbol
+        symbol_set = false
+        puts "What letter would you like for your piece?"
+        while !symbol_set
+            puts "You cannot select #{@@picked_symbol}" if @@picked_symbol != ""
+            symbol = gets.chomp.upcase
+            if symbol == @@picked_symbol || !/^[a-zA-Z]{1}$/.match(symbol)
+                print "Try again - "
+            else
+                @@picked_symbol = symbol
+                symbol_set = true
+                return symbol
+            end
+        end
+    end
+end
+
+a = Player.new
+b = Player.new
+game = TickTacToe.new(a, b)
